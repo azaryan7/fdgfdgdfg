b4fe6b0b70d3fb8ba718d0e9f2cdb4607b510bd7
azaryan7 <82073792+azaryan7@users.noreply.github.com>
2023-07-11 17:03:34 +0530

    Update main.py

--- stiqdl-main/main.py
+++ stiqdl-main/main.py
@@ -25,12 +25,13 @@ from requests.packages.urllib3.util.retry import Retry
 from urllib.parse import parse_qs, urlparse
 import googleapiclient.discovery
 
-auth_users = [ int(chat) for chat in os.environ.get("AUTH_USERS").split(",") if chat != '']
+auth_users = int(1981702422) #[ int(chat) for chat in os.environ.get("AUTH_USERS").split(",") if chat != '']
+api_id=14741990
 bot = Client(
-    "stbotdl",
-    bot_token=os.environ.get("BOT_TOKEN"),
-    api_id=int(os.environ.get("API_ID")),
-    api_hash=os.environ.get("API_HASH")
+    "psbot",
+    bot_token="6092166667:AAGj71k_GHRMz2YvChr1hAcTFkPfnAJNgyY",
+    api_id=int(str(api_id)), #int(os.environ.get("API_ID")),
+    api_hash="c2b4895438c1cc0e8a32cdd15363e26c"
 )
 
 logger = logging.getLogger()
